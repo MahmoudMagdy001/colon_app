8ce47c56607b1fb7b6d014b9427615bef0760337
MahmoudMagdi001 <urfavdodi@gmail.com>
2023-05-09 17:11:12 +0300

    finish

--- lib/features/gene_expression/presentation/views/widgets/gene_expression_details.dart
+++ lib/features/gene_expression/presentation/views/widgets/gene_expression_details.dart
@@ -1,13 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../../../constants.dart';
 import '../../../../../core/utlis/styles.dart';
 import '../../../../../core/widgets/custom_button.dart';
-
-import 'package:process_run/shell_run.dart';
 
 final supabase = Supabase.instance.client;
 
@@ -27,50 +23,6 @@ class _GeneDetailsState extends State<GeneDetails> {
     super.dispose();
     geneController.dispose();
   }
-
-  // Future<void> searchGene(String userInput) async {
-  //   final url =
-  //       Uri.parse('https://yrgwcqigxvexercvhhzr.functions.supabase.co/gene');
-
-  //   final header = {
-  //     'Content-Type': 'application/json',
-  //     'Authorization':
-  //         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyZ3djcWlneHZleGVyY3ZoaHpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwMTUwNzUsImV4cCI6MTk5NzU5MTA3NX0.n1jvJ8k-pUZtdA9OzrRyNBcJQ_FheFMoNTud3aBVK2Q'
-  //   };
-
-  //   final body = {'user_input_2': userInput};
-
-  //   final http.Response response =
-  //       await http.post(url, headers: header, body: json.encode(body));
-
-  //   if (response.statusCode == 200) {
-  //     final data = json.decode(response.body);
-  //     if (kDebugMode) {
-  //       print(data);
-  //     }
-  //   } else {
-  //     if (kDebugMode) {
-  //       print('Request failed with status: ${response.statusCode}.');
-  //     }
-  //   }
-  // }
-
-  // Future<String> invokeFunction() async {
-  //   final shell = Shell();
-
-  //   final result = await shell.run('assets/images/main2.py');
-
-  //   final data = json.decode(result.outText);
-  //   return data;
-  //   //  else {
-  //   //   throw Exception('Failed to invoke function: $result');
-  //   // }
-  // }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  // }
 
   @override
   Widget build(BuildContext context) {
